e5724b06dc7f0dbc1e577b84c35e0f19b9b4719a
Shubham Agarwal <mwshubham@gmail.com>
2019-11-12 11:32:01 +0530

    added app component added dagger dependencies annotation processor and kapt

---
+++ app/src/main/java/com/example/daggerdemo/di/AppComponent.kt
@@ -0,0 +1,29 @@
+package com.example.daggerdemo.di
+
+import android.app.Application
+import com.example.daggerdemo.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+
+/**
+ * [AppComponent] as a Server
+ * [BaseApplication] as a Client
+ *
+ * Reference: {@link https://www.youtube.com/watch?v=31vswk6r8FA&list=PLgCYzUzKIBE8AOAspC3DHoBNZIBHbIOsC&index=6}
+ */
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+
+        fun build(): AppComponent
+    }
+}
